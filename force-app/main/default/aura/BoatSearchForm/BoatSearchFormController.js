0710637a5e1400df39cd5d7eb366200fbd151366
Debora Ribeiro Silva <debora.silva@bexpert.lan.corp>
2021-09-08 14:00:11 -0300

    Build the Search Form

--- force-app/main/default/aura/BoatSearchForm/BoatSearchFormController.js
+++ force-app/main/default/aura/BoatSearchForm/BoatSearchFormController.js
@@ -1,5 +1,28 @@
 ({
-    myAction : function(component, event, helper) {
+    initBoatSearchForm : function(component, event, helper) {
+        var createRecordEvent = $A.get("e.force:createRecord");
+        if (createRecordEvent){
+            component.set("v.showNewButton", true);
+        }
+        
 
-    }
+    },
+    
+    createNewBoat : function(component, event, helper) {
+        var boatTypeId = component.find("boatTypeSelect").get("v.value");
+        
+        var params = {"entityApiName": "Boat__c"};
+        if(boatTypeId){
+            params.defaultFieldValues = {"BoatType__c" : boatTypeId};
+        }
+        
+        var createBoatRecordEvent = $A.get("e.force:createRecord");
+        createBoatRecordEvent.setParams(params);
+        
+        createAcountContactEvent.fire();
+        
+    },
+    
+
+    
 })
